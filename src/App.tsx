afa26965133a36d6caab492f814e4ed0f59d59bd
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-03-11 15:16:06 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex justify-center ">
+      <div className=" justify-center">
+        <h1 className="text-5xl text-sky-400 mb-5 mt-5">Welcome </h1>
+        <Button>Arfin Mia</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
